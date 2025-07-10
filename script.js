76e071cd5f9e79bee7c9226b62ec70511fb6c6cf
Harshitaa-1 <harshitatyagi025@gmail.com>
2025-07-10 16:57:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+  let slides = document.querySelectorAll('.carousel-slide');
+  let currentIndex = 0;
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.classList.toggle('active', i === index);
+    });
+  }
+
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+  }
+
+  setInterval(nextSlide, 3000); // Change slide every 3 seconds
